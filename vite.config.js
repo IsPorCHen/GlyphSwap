9db7b487d5e08a2f0a3f40ad9ed119a89ecae163
IsPorCHen <fazy2019@gmail.com>
2026-05-24 09:36:59 +0500

    Update README

--- vite.config.js
+++ vite.config.js
@@ -13,10 +13,15 @@ export default defineConfig({
             userscript: {
                 name: 'GlyphSwap',
                 namespace: 'https://github.com/IsPorCHen/GlyphSwap',
+                homepage: 'https://github.com/IsPorCHen/GlyphSwap',
+                supportURL: 'https://github.com/IsPorCHen/GlyphSwap/issues',
+                downloadURL: 'https://github.com/IsPorCHen/GlyphSwap/releases/latest/download/glyphswap.user.js',
+                updateURL: 'https://github.com/IsPorCHen/GlyphSwap/releases/latest/download/glyphswap.user.js',
+
                 version: pkg.version,
                 author: pkg.author,
                 description: pkg.description,
-                icon: 'https://cdn-icons-png.flaticon.com/512/3203/3203875.png',
+                icon: 'https://github.com/IsPorCHen/GlyphSwap/raw/main/assets/logo.png',
                 match: ['*://*/*'],
                 grant: ['GM_xmlhttpRequest'],
                 connect: ['translate.yandex.net', '*'],
